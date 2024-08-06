762c27898d17968bc06462a4962bcfb25f59c768
Lucas Roig <lucas.roig1@gmail.com>
2024-08-06 21:15:58 +0200

    fun with types

---
+++ src/query-builder/lib.ts
@@ -0,0 +1,43 @@
+type StringField<N extends string> = {
+  name: N;
+  type: 'string';
+}
+
+export class QueryBuilder<Fields extends object = {}> {
+  private fields: Fields;
+
+  constructor(fields: Fields) {
+    this.fields = fields ?? {};
+  }
+  stringField<N extends string>(name: N) {
+    return new QueryBuilder<Fields & { [K in N]: StringField<K> }>({
+      ...this.fields,
+      [name]: { name, type: 'string' }
+    } as Fields & { [K in N]: StringField<K> });
+  }
+}
+
+export type StringFieldsName<T extends QueryBuilder> = T extends QueryBuilder<infer R> ? {
+  [K in keyof R]: R[K] extends StringField<infer N> ? N : never;
+}[keyof R] : never;
+
+export type StringCondition<T extends QueryBuilder> = StringBinaryCondition<T> | StringArrayCondition<T>;
+
+export type StringBinaryCondition<T extends QueryBuilder> = {
+  fieldName: StringFieldsName<T>;
+  operator: 'eq' | 'neq' | 'contains' | 'startsWith' | 'endsWith';
+  value: string;
+}
+
+export type StringArrayCondition<T extends QueryBuilder> = {
+  fieldName: StringFieldsName<T>;
+  operator: 'in' | 'notIn';
+  values: string[];
+}
+
+type Combinator<T extends QueryBuilder> = {
+  operator: 'and' | 'or';
+  conditions: ConditionTree<T>[];
+}
+
+export type ConditionTree<T extends QueryBuilder> = StringCondition<T> | Combinator<T>;
---
+++ src/query-builder/playground.ts
@@ -0,0 +1,83 @@
+import { match } from "ts-pattern";
+import { ConditionTree, QueryBuilder, StringCondition, StringFieldsName } from "./lib";
+
+const qb = new QueryBuilder({}).stringField('name').stringField('age');
+
+type stringFields = StringFieldsName<typeof qb>;
+
+type stringCondition = StringCondition<typeof qb>;
+// declare const decStringCondition: StringCondition<typeof qb>;
+
+// if (decStringCondition.operator === 'in') {
+//   decStringCondition.values
+// }
+
+// if (decStringCondition.operator === 'eq') {
+//   decStringCondition.value
+// }
+
+// declare const conditionTree: ConditionTree<typeof qb>;
+
+// if (conditionTree.operator === 'and') {
+//   conditionTree.conditions
+// }
+
+// if (conditionTree.operator === 'in') {
+//   conditionTree.values
+// }
+
+// if (conditionTree.operator === 'eq') {
+//   conditionTree.value
+// }
+
+function queryTreeVisitor(conditionTree: ConditionTree<typeof qb>): string {
+  return match(conditionTree)
+    .with({operator: 'and'}, q => `AND(${q.conditions.map(queryTreeVisitor).join(', ')})`)
+    .with({operator: 'or'}, q => `OR(${q.conditions.map(queryTreeVisitor).join(', ')})`)
+    .with({operator: 'eq'}, q => `${q.fieldName} = ${q.value}`)
+    .with({operator: 'neq'}, q => `${q.fieldName} != ${q.value}`)
+    .with({operator: 'contains'}, q => `${q.fieldName} LIKE '%${q.value}%'`)
+    .with({operator: 'startsWith'}, q => `${q.fieldName} LIKE '${q.value}%'`)
+    .with({operator: 'endsWith'}, q => `${q.fieldName} LIKE '%${q.value}'`)
+    .with({operator: 'in'}, q => `${q.fieldName} IN (${q.values.map(v => `'${v}'`).join(', ')})`)
+    .with({operator: 'notIn'}, q => `${q.fieldName} NOT IN (${q.values.map(v => `'${v}'`).join(', ')})`)
+    .exhaustive();
+}
+
+const tree = {
+  operator: 'and',
+  conditions: [
+    {
+      operator: 'or',
+      conditions: [
+        {
+          operator: 'eq',
+          fieldName: 'name',
+          value: 'John'
+        },
+        {
+          operator: 'eq',
+          fieldName: 'age',
+          value: '20'
+        }
+      ]
+    },
+    {
+      operator: 'and',
+      conditions: [
+        {
+          operator: 'eq',
+          fieldName: 'name',
+          value: 'John'
+        },
+        {
+          operator: 'eq',
+          fieldName: 'age',
+          value: '20'
+        }
+      ]
+    }
+  ]
+} satisfies ConditionTree<typeof qb>;
+
+export const queryString = queryTreeVisitor(tree);
